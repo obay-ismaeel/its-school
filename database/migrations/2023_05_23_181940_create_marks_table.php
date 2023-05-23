1362b9098b8734700d138400484f295f9d48bbd0
Obay Ismaeel <obayhany@gmail.com>
2023-05-23 14:57:23 -0400

    Created tables connected to students table

---
+++ database/migrations/2023_05_23_181940_create_marks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('marks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->constrained();
+            $table->foreignId('grade_course_id')->constrained('grade_coures');
+
+            $table->enum('type', ['practical','exam','test']);
+            $table->integer('score');
+            $table->year('year');
+            $table->enum('term',['first','second']);
+            $table->unique(['year','term','type','student_id','grade_course_id']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('marks');
+    }
+};
---
+++ database/migrations/2023_05_23_183320_create_totals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('totals', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->constrained();
+            $table->foreignId('grade_course_id')->constrained();
+
+            $table->year('year');
+            $table->integer('first_term_score');
+            $table->integer('second_term_score');
+            $table->integer('final_score');
+            $table->boolean('has_failed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('totals');
+    }
+};
